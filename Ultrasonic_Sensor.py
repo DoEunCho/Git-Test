21d8f1c3bdd0eebeb129acad30b1b82e1e728111
DoEunCho <48686652+DoEunCho@users.noreply.github.com>
2023-11-24 13:52:43 +0900

    Add files via upload

---
+++ Ultrasonic_Sensor.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO                    
+import time                                
+
+Trig = 4
+Echo = 17
+
+GPIO.setmode(GPIO.BCM)                    
+GPIO.setup(Trig, GPIO.OUT)                   # Trig=4 초음파 신호 전송핀 번호 지정 및 출력지정
+GPIO.setup(Echo, GPIO.IN)                    # Echo=17 초음파 수신 핀 번호 지정 및 입력지정
+
+print “Press SW or input Ctrl+C to quit”   # 메세지 화면 출력
+
+try:
+    while True:
+            GPIO.output(Trig, False)         # Trig핀 초기 설정
+            time.sleep(0.5)                    # 0.5초 지연
+
+            GPIO.output(Trig, True)          # Trig핀에 펄스 출력
+            time.sleep(0.00001)              # 시간 지연
+            GPIO.output(Trig, False) 
+            while GPIO.input(Echo) == 0:     # Echo핀 입력 없을 경우
+               start = time.time()                # 시간 저장
+
+            while GPIO.input(Echo) == 1:     # Echo핀 입력 있을 경우
+               stop = time.time()
+
+            time_interval = stop – start          # 초음파가 수신되는 시간 간격 저장
+            distance = time_interval * 17000   # 거리를 계산
+            distance = round(distance, 2)       # 소수점 2자리까지 반올림
+
+            print “Distance => “, distance, “cm”
+
+except KeyboardInterrupt:                  # Ctrl-C 입력 시
+    GPIO.cleanup()                         # GPIO 관련설정 Clear
+    Print "bye~"
